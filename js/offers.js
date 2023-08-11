0c30736d21937c3afdeefcd62ae6dde403fd4d1a
Ravi Chaudhary <ravi9559@users.noreply.github.com>
2023-08-11 07:17:42 +0530

    first commit

---
+++ js/offers.js
@@ -0,0 +1,105 @@
+const urlParams = new URLSearchParams(window.location.search);
+  const breed = urlParams.get("category");
+  let currentPage = 1;
+  let images = [];
+  const imagesPerPage=10;
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const breedContainer = document.getElementById("breedImagesContainer");
+
+    fetchImages(currentPage);
+
+    function createBreedCard(images) {
+      const breedCard = document.createElement("div");
+      breedCard.className = "container mt-5";
+
+      const startIndex = (currentPage - 1) * imagesPerPage;
+      const endIndex = startIndex + imagesPerPage;
+      const slicedImages = images.slice(startIndex, endIndex);
+
+      breedCard.innerHTML = `
+        <div class="row m-3"> 
+          <div class="col">
+            <h3 class="text-capitalize">${breed}</h3>
+          </div>
+        </div>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-5 g-3">
+          ${generateImageCards(slicedImages)}
+        </div>
+        <div class="row m-3">
+          <div class="col">
+            ${generatePagination(images)}
+          </div>
+        </div>
+      `;
+      return breedCard;
+    }
+
+    function generateImageCards(images) {
+      let imageCardsHTML = "";
+
+      for (let j = 0; j < images.length; j++) {
+        const imageSrc = images[j];
+
+        imageCardsHTML += `
+          <div class="col">
+            <div class="card">
+              <img src="${imageSrc}" class="card-img-top" alt="Image ${j + 1}" style="height: 300px; object-fit: cover;">
+              <div class="card-body">
+                <h5 class="card-title">Card ${j + 1} Title</h5>
+                <p class="card-text">Description for Card ${j + 1}.</p>
+              </div>
+            </div>
+          </div>
+        `;
+      }
+
+      return imageCardsHTML;
+    }
+
+    function generatePagination(images) {
+      const totalPages = Math.ceil(images.length / imagesPerPage);
+
+      let paginationHTML = `
+        <nav>
+          <ul class="pagination justify-content-center">
+      `;
+
+      for (let i = 1; i <= totalPages; i++) {
+        paginationHTML += `
+          <li class="page-item"><a class="page-link" href="#" onclick="handlePagination(${i})">${i}</a></li>
+        `;
+      }
+
+      paginationHTML += `
+          </ul>
+        </nav>
+      `;
+
+      return paginationHTML;
+    }
+
+    window.handlePagination = function(page) {
+      currentPage = page;
+      breedContainer.innerHTML = '';
+      breedContainer.appendChild(createBreedCard(images));
+    };
+
+    function fetchImages(page) {
+      fetch(`https://dog.ceo/api/breed/${breed}/images`)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.status !== "error") {
+            images = data.message.filter(
+              (image) => !image.includes("Not Found")
+            );
+            handlePagination(page); // Display the specified page
+          } else {
+            console.error(
+              `Error fetching images for ${breed}:`,
+              data.message
+            );
+          }
+        });
+    }
+  });
---
+++ js/script.js
@@ -0,0 +1,105 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const breedContainer = document.getElementById("breedContainer");
+
+  // Fetch breed list
+  fetch("https://dog.ceo/api/breeds/list/all")
+    .then((response) => response.json())
+    .then((data) => {
+      const breeds = Object.keys(data.message);
+      breeds.forEach((breed) => {
+        fetch(`https://dog.ceo/api/breed/${breed}/images`)
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.status !== 404) {
+              const images = data.message.filter(
+                (image) => !image.includes("Not Found")
+              );
+              const breedCard = createBreedCard(breed, images);
+              breedContainer.appendChild(breedCard);
+            } else {
+              console.error(
+                `Error fetching images for ${breed}:`,
+                data.message
+              );
+            }
+          })
+          .catch((error) => {
+            console.error(`Error fetching images for ${breed}:`, error);
+          });
+      });
+    });
+
+  // Create breed card with name and images
+  function createBreedCard(breed, images) {
+    const breedCard = document.createElement("div");
+    breedCard.className = "container mt-5";
+    breedCard.innerHTML = `
+      <div class="row m-3 px-5"> 
+        <div class="col">
+          <h3 class="text-capitalize">${breed}</h3>
+        </div>
+        <div class="col">
+          
+          <a href="${window.location.origin}/offers.html?category=${encodeURIComponent(breed)}" class="btn btn-danger float-end">View more</a>
+          </div>
+        </div>
+      </div>
+      <div id="${breed}-slider" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          ${generateCarouselItems(images)}
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#${breed}-slider" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#${breed}-slider" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    `;
+    return breedCard;
+  }
+
+  function generateCarouselItems(images) {
+    let slideshowHTML = "";
+    const maxImagesToShow = Math.min(images.length, 15);
+
+    for (let i = 0; i < maxImagesToShow; i += 5) {
+      const isActive = i === 0 ? "active" : "";
+
+      slideshowHTML += `
+            <div class="carousel-item ${isActive}">
+
+            <div class="card-group  mx-5 ">
+        `;
+
+      for (let j = i; j < i + 5 && j < maxImagesToShow; j++) {
+        const imageSrc = images[j];
+
+        slideshowHTML += `
+
+                    <div class="card me-3">
+                        <img src="${imageSrc}" class="img-fluid" alt="Image ${
+          j + 1
+        }" style="width: 400px; height: 300px; object-fit: cover;">
+                        <div class="card-body">
+                            <h5 class="card-title">Card ${j + 1} Title</h5>
+                            <p class="card-text">Description for Card ${
+                              j + 1
+                            }.</p>
+                        </div>
+                    </div>
+
+            `;
+      }
+
+      slideshowHTML += `
+                </div>
+            </div>
+        `;
+    }
+
+    return slideshowHTML;
+  }
+});
